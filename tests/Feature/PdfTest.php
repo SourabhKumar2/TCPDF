3533e7da51f5c5b37d5618922875b39399ededa8
Sourabh <sourabhkumar2405@gmail.com>
2020-03-14 18:18:55 +0530

    Add tests and coverage

--- tests/Feature/PdfTest.php
+++ tests/Feature/PdfTest.php
@@ -2,10 +2,18 @@
 
 namespace Feature;
 
-
-use TestCase;
+use PHPUnit\Framework\TestCase;
+use TCPDF;
 
 class PdfTest extends TestCase
 {
+    /** @test */
+    public function can_stream_pdf()
+    {
+        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
+        $output = $pdf->Output('', 'S');
+
+        $this->assertStringStartsWith('%PDF-', $output);
+    }
 }
