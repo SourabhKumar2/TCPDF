45a2001976c1b0b5f8a94669c9e076e4f820cfd3
Sourabh <sourabhkumar2405@gmail.com>
2020-03-14 21:14:30 +0530

    upgrade project to php7.1 and add test for no valid destination

--- tests/Feature/PdfTest.php
+++ tests/Feature/PdfTest.php
@@ -7,13 +7,37 @@ use TCPDF;
 
 class PdfTest extends TestCase
 {
-    /** @test */
-    public function can_stream_pdf()
-    {
-        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    /**
+     * @var TCPDF
+     */
+    private $pdf;
 
-        $output = $pdf->Output('', 'S');
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    }
+
+    /** @test */
+    public function can_recieve_pdf_as_string()
+    {
+        $output = $this->pdf->Output('', 'S');
 
         $this->assertStringStartsWith('%PDF-', $output);
+    }
+
+    /** @test */
+    public function it_throws_an_exception_when_provided_invalid_destination(): void
+    {
+        $destination= 'invalid';
+        try {
+            $this->pdf->Output('', $destination);
+        } catch (\Exception $exception) {
+            $this->assertEquals('TCPDF ERROR: Incorrect output destination: '. strtoupper($destination), $exception->getMessage());
+            return;
+        }
+
+        $this->fail('Exception is not thrown when given destination '. $destination);
     }
 }
